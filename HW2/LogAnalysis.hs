f1ca709ef91516305c08b3765ad3c8d0ad281daf
readbrent <readbrent@gmail.com>
2017-08-21 21:27:10 -0700

    Added in the insertion function

--- HW2/LogAnalysis.hs
+++ HW2/LogAnalysis.hs
@@ -21,3 +21,20 @@ parseWarningMessage (timeStamp : message) =
 parseErrorMessage :: [String] -> LogMessage
 parseErrorMessage (id : level : message) =
     LogMessage (Error (read id)) (read level) (unwords message)
+
+parse :: String -> [LogMessage]
+parse str = map parseMessage (lines str)
+
+getTimestamp :: LogMessage -> TimeStamp
+getTimestamp (Unknown _) = error "Invalid input"
+getTimestamp (LogMessage Info      timestamp _) = timestamp
+getTimestamp (LogMessage Warning   timestamp _) = timestamp
+getTimestamp (LogMessage (Error _) timestamp _) = timestamp
+
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert msg Leaf = Node Leaf msg Leaf
+insert newMessage (Node left currentMessage right)
+    | getTimestamp newMessage <= getTimestamp currentMessage = Node (insert newMessage left) currentMessage right
+    | otherwise                                              = Node left currentMessage (insert newMessage right)
--- HW2/LogTests.hs
+++ HW2/LogTests.hs
@@ -7,12 +7,16 @@ import Log
 main :: IO ()
 main = runTestTT tests >>= print
 
-test1 = TestCase $ assertEqual "" (LogMessage (Error 2) 562 "help help") (parseMessage "E 2 562 help help")
-
-test2 = TestCase $ assertEqual "" (LogMessage Info 29 "La La La") (parseMessage "I 29 La La La")
-
-test3 = TestCase $ assertEqual "" (Unknown "blah blah blah") (parseMessage "blah blah blah")
-
 tests = TestList [TestLabel "test1" test1,
                  TestLabel "test2" test2,
                  TestLabel "test3" test3]
+
+
+test1 = TestCase $ assertEqual "" (LogMessage (Error 2) 562 "help help") (parseMessage "E 2 562 help help")
+test2 = TestCase $ assertEqual "" (LogMessage Info 29 "La La La") (parseMessage "I 29 La La La")
+test3 = TestCase $ assertEqual "" (Unknown "blah blah blah") (parseMessage "blah blah blah")
+
+
+testInsertUnknown = TestCase $ assertEqual "" (insert (Unknown "") Leaf) (Leaf)  
+
+infoMessage n = LogMessage Info n ""
