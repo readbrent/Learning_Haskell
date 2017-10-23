9294d8f44e80d7e136b1fe174a94341b32c41643
readbrent <readbrent@gmail.com>
2017-10-22 19:23:52 -0700

    Starting Assignment 7

---
+++ HW7/JoinList.hs
@@ -0,0 +1,18 @@
+module JoinList where
+
+import Data.Monoid
+
+data JoinList m a = Empty
+    | Single m a
+    | Append m (JoinList m a) (JoinList m a)
+    deriving (Eq, Show)
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty = mempty
+tag (Single m _) = m
+tag (Append m _ _) = m
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) Empty a = a
+(+++) a Empty = a
+(+++) a b = Append ((tag a) <> (tag b)) a b
